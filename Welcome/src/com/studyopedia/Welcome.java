3093c6486f6a65427a1a67c05c2b47ebe02a38ee
User <User@LAPTOP-QUFJE5SL>
2023-10-16 01:12:55 +0800

    First Commit

---
+++ Welcome/src/com/studyopedia/Welcome.java
@@ -0,0 +1,10 @@
+package com.studyopedia;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("Java");
+        System.out.println("Programming !");
+    }
+}
+
